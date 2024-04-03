aee41697be0d1510999356145ae538721580f22b
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-04-03 12:07:57 +0300

    pagination

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const navLink = document.querySelectorAll(".nav_link");
+const card = document.querySelectorAll(".card");
 
 navLink.forEach(function (element) {
   element.addEventListener("click", active);
@@ -8,12 +9,20 @@ navLink.forEach(function (element) {
 
 function active(evt) {
   const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
 
   navLink.forEach(function (item) {
     item.classList.remove("nav_link-active");
   });
 
+  card.forEach(function (item) {
+    item.classList.remove("content-item--active");
+  });
+
   tabTarget.classList.add("nav_link-active");
+  document
+    .querySelector(`#${button}`)
+    .classList.add("content-item--active");
 }
 
 const btnTh = document.querySelector(".btn_th");
